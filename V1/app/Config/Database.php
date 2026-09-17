de20866aab851c1486e0404290c5bed47e7831f6
LegacyAngel2K9 <djangel@live.no>
2026-09-17 13:02:00 +0200

    Remove hardcoded database credentials
    Replace production database hostname, username, password, and database name with placeholders in the default configuration.

--- V1/app/Config/Database.php
+++ V1/app/Config/Database.php
@@ -27,10 +27,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'db.legacyhosting.xyz',
-        'username'     => 'lega_tg_logistics_cms',
-        'password'     => 'A7kP3xQ9mT2Lr8Vz',
-        'database'     => 'lega_tg_logistics_cms',
+        'hostname'     => 'REPLACE',
+        'username'     => 'REPLACE',
+        'password'     => 'REPLACE',
+        'database'     => 'REPLACE',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
